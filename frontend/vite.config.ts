1670b40bb4fb84492f5e26f061f4673e1ab0c39e
arp-aiml <pranay.nath@aimill.in>
2026-02-19 01:48:23 +0530

    Update API proxy target in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://aimilltest-dbd6dvhdcceef3d4.westus2-01.azurewebsites.net',
+        target: 'https://mc4-forecasting-backend-hne0aufgdqdhf6a4.westus2-01.azurewebsites.net',
         changeOrigin: true,
       },
     },
